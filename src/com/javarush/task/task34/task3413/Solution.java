fb7c5f302a587f620eadf77adac08b731b747a6d
Pawek <juebok16@gmail.com>
2026-01-23 20:50:33 +0300

    3x feature

--- src/com/javarush/task/task34/task3413/Solution.java
+++ src/com/javarush/task/task34/task3413/Solution.java
@@ -8,6 +8,8 @@ public class Solution {
     public static void main(String[] args) {
         SoftCache cache = new SoftCache();
         System.out.println("feature");
+        System.out.println("feature");
+        System.out.println("feature");
         for (long i = 0; i < 2_500_000; i++) {
             cache.put(i, new AnyObject(i, null, null));
         }
