4efe855c07911a6218be02ec1021fc4214d866ca
Pawek <juebok16@gmail.com>
2026-01-25 15:30:47 +0300

    first cherry-pick

--- src/com/javarush/task/task34/task3413/Solution.java
+++ src/com/javarush/task/task34/task3413/Solution.java
@@ -12,6 +12,8 @@ public class Solution {
         System.out.println("feature");
         System.out.println("feature");
         System.out.println("feature");
+        System.out.println("feature1");
+        System.out.println("feature2");
         for (long i = 0; i < 2_500_000; i++) {
             System.out.println("main branch");
             cache.put(i, new AnyObject(i, null, null));
