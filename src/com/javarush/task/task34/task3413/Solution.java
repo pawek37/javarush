b3e08c16fdab7eb5e54fe8f987a3cbc51803bf7f
Pawek <juebok16@gmail.com>
2026-01-25 16:29:11 +0300

    2 line conflict

--- src/com/javarush/task/task34/task3413/Solution.java
+++ src/com/javarush/task/task34/task3413/Solution.java
@@ -15,7 +15,9 @@ public class Solution {
         System.out.println("feature1");
         System.out.println("feature2");
         for (long i = 0; i < 2_500_000; i++) {
+            System.out.println("test");
             System.out.println("main branch");
+            System.out.println("test");
             cache.put(i, new AnyObject(i, null, null));
         }
     }
