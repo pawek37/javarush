f08d01fcf03db6c3a5815d502c7a35c0b70df563
Pawek <juebok16@gmail.com>
2026-01-25 16:53:35 +0300

    after revert

--- src/com/javarush/task/task34/task3413/Solution.java
+++ src/com/javarush/task/task34/task3413/Solution.java
@@ -20,7 +20,6 @@ public class Solution {
             System.out.println("test");
             System.out.println("main branch2");
             System.out.println("adapted");
-            System.out.println("useless");
             cache.put(i, new AnyObject(i, null, null));
         }
     }
